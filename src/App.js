c0965fc024fd8ae4ee3f23ff6ac11a7801d535ad
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-10-02 17:22:36 +0300

    Added FormattedMessage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Summary from './charts/summary';
-import './App.css';
+import { FormattedMessage } from 'react-intl';
 
 function App() {
   return (
     <div className="App-header">
-      <h1 className="font-mono tracking-widest dark:text-pink-500">All your Covid-19 information here</h1>
+      <h1 className="font-mono tracking-widest dark:text-pink-500">
+        <FormattedMessage 
+          id='app.header'
+          defaultMessage='All your Covid-19 information here' />  
+      </h1>
       <Summary/>
     </div>
   );
