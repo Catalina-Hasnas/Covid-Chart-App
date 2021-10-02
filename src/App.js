9b9cb1ddcec1cb236bfa27a93edc1fb3b8c6ce9a
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-10-02 20:50:20 +0300

    added react query

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import Summary from './charts/summary';
 import { FormattedMessage } from 'react-intl';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 function App() {
+
+  const queryClient = new QueryClient()
+
   return (
-    <div className="App-header">
+    <main className="my-auto">
       <h1 className="font-mono tracking-widest dark:text-pink-500">
         <FormattedMessage 
           id='app.header'
           defaultMessage='All your Covid-19 information here' />  
       </h1>
-      <Summary/>
-    </div>
+      <QueryClientProvider client={queryClient}>
+        <Summary/>
+      </QueryClientProvider>
+    </main>
   );
 }
 
--- src/charts/summary.js
+++ src/charts/summary.js
@@ -2,46 +2,49 @@ import React, { Fragment, useEffect, useState } from 'react';
 import axios from '../services/api';
 import Bar from '../Chart/Bar';
 import { iterateObject, formatString, formatNumber, formatDate } from '../services/const';
+import { useQuery } from 'react-query';
 
 
 const Summary = () => {
 
-    const [Summary, setSummary] = useState({});
-    const [RomaniaSummary, setRomaniaSummary] = useState({});
-    const [DateToday, setDateToday] = useState('')
+    const { data, isLoading, isError, error } = useQuery('summary', async () => { return await axios.get('/summary') });
 
-    const fetchSummary = async () => {
-        const data = await axios.get('/summary');
-        setSummary(setObjectInfo(data.data.Global));
-        const romaniaSummary = data.data.Countries.find(obj => {
-            return obj.Country === 'Romania'
-        })
-        setRomaniaSummary(setObjectInfo(romaniaSummary));
-        setDateToday(formatDate(data.data.Global.Date));
+    if (isLoading) {
+
+        return <span> Loading... </span>
+        
+    };
+
+    if (isError) {
+
+        return <span> { error.message } </span>
+
     };
 
     const setObjectInfo = (obj) => {
-        return {
-            NewConfirmed: obj.NewConfirmed,
-            TotalConfirmed: obj.TotalConfirmed,
-            NewDeaths: obj.NewDeaths,
-            TotalDeaths: obj.TotalDeaths,
-            NewRecovered: obj.NewRecovered,
-            TotalRecovered: obj.TotalRecovered
+        const properties = ['NewConfirmed', 'TotalConfirmed', 'NewDeaths', 'TotalDeaths', 'NewRecovered', 'TotalRecovered'];
+        const result = {};
+        for (const key of properties) {
+            result[key] = obj[key];
         }
-    }
+        return result
+    };
 
-    useEffect(() => {
-        fetchSummary(DateToday)
-    }, []);
-    
+    const globalInfo = setObjectInfo(data.data.Global);
+
+    const romaniaInfo = setObjectInfo(data.data.Countries.find(obj => {
+        return obj.Country === 'Romania'
+    })); 
+
+    const dateToday = formatDate(data.data.Global.Date);
+
     return (
         <Fragment>
-            <div className="w-2/5 mt-5 mx-auto flex justify-around shadow-inner rounded-md">
-                <Stat type = {'summary'} keysValues = {iterateObject(Summary, true)}/>
-                <Stat type = {'romaniaSummary'} keysValues = {iterateObject(RomaniaSummary, true)}/>
+            <div className="w-3/5 mt-5 mx-auto flex justify-around shadow-inner rounded-md">
+                <Stat type = {'summary'} keysValues = {iterateObject(globalInfo, true)}/>
+                <Stat type = {'romaniaSummary'} keysValues = {iterateObject(romaniaInfo, true)}/>
             </div>
-            <p> {DateToday} </p> 
+            <p> {dateToday} </p> 
         </Fragment>
     )
 }
@@ -49,16 +52,18 @@ const Summary = () => {
 export default Summary
 
 const Stat = (props) => {
+    console.log(props);
     return (
-        <div>
+        <div className='flex flex-col'>
             {props.type === 'summary' ? <p> Worldwide: </p> : <p> Romania: </p>}
-            {props.keysValues.map((key, index) =>  (
-                <div key={index}>
-                    <span className="dark:text-pink-500 text-xs"> {formatString(key[0])}: </span>
-                    <span className="text-xs">  {formatNumber(key[1])} </span>
-                </div>
-                ))
-            }
+            <div className='text-left leading-relaxed ml-12'>
+                {props.keysValues.map((key, index) => (
+                    <div key={index}>
+                        <span className='text-pink-500'> {formatString(key[0])}: </span>
+                        <span> {formatNumber(key[1])} </span>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
