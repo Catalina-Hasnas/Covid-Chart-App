5d353181cc79bc96e640768bd17f1a3803819da3
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-11-03 19:25:28 +0200

    added more routes with lazy loading

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
-import React from 'react';
-import Summary from './charts/summary';
+import React, {lazy} from 'react';
 import { FormattedMessage } from 'react-intl';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { ReactQueryDevtools } from 'react-query/devtools'
+
+const RomaniaPromise = import ('./charts/romania/romania');
+const SummaryPromise = import ('./charts/summary');
+
+const Romania = lazy(()=> RomaniaPromise);
+const Summary = lazy(()=> SummaryPromise);
 
 function App() {
 
   const queryClient = new QueryClient()
 
   return (
-    <>
-      <main className="mt-5">
-        <h1 className="font-mono tracking-widest text-4xl font-extrabold dark:text-pink-500">
-          <FormattedMessage 
-            id='app.header'
-            defaultMessage='All your Covid-19 information here' />  
-        </h1>
+    <React.Suspense fallback={<div>Loading suspense...</div>}>
+      <Router>
         <QueryClientProvider client={queryClient}>
-          <Summary/>
+          <main className="mt-5">
+            <h1 className="font-mono tracking-widest text-4xl font-extrabold text-pink-500">
+              <FormattedMessage 
+                id='app.header'
+                defaultMessage='All your Covid-19 information here' />  
+            </h1>
+              <Switch>
+              <Route path="/Romania">
+                <Romania />
+              </Route>
+              <Route path="/">
+                <Summary />
+              </Route>
+            </Switch>
+          </main>
         </QueryClientProvider>
-      </main>
-      {/* <ReactQueryDevtools initialIsOpen={false}/> */}
-    </>
+        {/* <ReactQueryDevtools initialIsOpen={false}/> */}
+      </Router>
+    </React.Suspense>
   );
 }
 
---
+++ src/charts/romania/romania.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Romania = () => {
+    return (
+        <div>
+            some
+        </div>
+    )
+}
+
+export default Romania
