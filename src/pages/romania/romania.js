cf5a52aacb8f0a821c4b852cbbf17ce036851cee
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-11-05 21:13:46 +0200

    added get cases by month

--- src/pages/romania/romania.js
+++ src/pages/romania/romania.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import { useLocation } from 'react-router';
 import axios from '../../services/api';
 import { useQuery } from 'react-query';
@@ -9,67 +9,74 @@ import { formatDate } from '../../services/const';
 const Romania = () => {
 
     const { data, isLoading, isError, error } = useQuery('romaniaOctober', async () => { 
-        return await axios.get('/country/Romania/status/confirmed?from=2021-09-01T00:00:00Z&to=2021-10-01T00:00:00Z') 
+      return await axios.get('/country/Romania/status/confirmed?from=2020-02-01T00:00:00Z&to=2020-12-31T00:00:00Z') 
     });
 
     if (isLoading) {
 
-        return <span> Loading... </span>
+      return <span> Loading... </span>
         
     };
 
     if (isError) {
 
-        return <span> { error.message } </span>
+      return <span> { error.message } </span>
 
     };
 
     console.log(data);
 
+    const getAllMonthlyCases = () => {
+      let months = [
+        { name: 'February',
+          nr: 2 },
+        { name: 'March',
+          nr: 3 },
+        { name: 'April',
+          nr: 4 },
+      ];
+      let monthsWithDays = [];
+      months.map((month) => {
+        let oneMonth = {
+          name: month.name, 
+          days: data.data.filter(obj => obj.Date.includes(`2020-0${month.nr}`))
+        };
+          monthsWithDays = [...monthsWithDays, oneMonth ];
+        })
+      return monthsWithDays
+    } 
+
+    console.log(getAllMonthlyCases())
 
 
     const getDays = () => {
-        let arrOfDays = []
-        data.data.map((day) => {
-            arrOfDays = [...arrOfDays, day.Date]
-        })
-        return arrOfDays
+      let arrOfDays = []
+      data.data.map((day) => {
+        arrOfDays = [...arrOfDays, formatDate(day.Date)]
+      })
+      return arrOfDays
     } 
 
     const getCases = () => {
         let arrOfCases = [];
         data.data.map((day) => {
-            arrOfCases = [...arrOfCases, day.Cases]
+          arrOfCases = [...arrOfCases, day.Cases]
         })
         return arrOfCases
     }
 
     const chartData = {
-        labels: getDays(),
-        datasets: [
-          {
-            label: '# of Cases',
-            data: getCases(),
-            // backgroundColor: [
-            //   'rgba(255, 99, 132, 0.2)',
-            //   'rgba(54, 162, 235, 0.2)',
-            //   'rgba(255, 206, 86, 0.2)',
-            //   'rgba(75, 192, 192, 0.2)',
-            //   'rgba(153, 102, 255, 0.2)',
-            //   'rgba(255, 159, 64, 0.2)',
-            // ],
-            // borderColor: [
-            //   'rgba(255, 99, 132, 1)',
-            //   'rgba(54, 162, 235, 1)',
-            //   'rgba(255, 206, 86, 1)',
-            //   'rgba(75, 192, 192, 1)',
-            //   'rgba(153, 102, 255, 1)',
-            //   'rgba(255, 159, 64, 1)',
-            // ],
-            // borderWidth: 1,
-          },
-        ],
-      };
+      labels: getDays(),
+      datasets: [
+        {
+          label: 'nr of Cases',
+          data: getCases(),
+          backgroundColor:'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1,
+        },
+      ],
+    };
       
       const options = {
         scales: {
@@ -85,14 +92,18 @@ const Romania = () => {
       
 
     return (
-        <div>
-            <Bar 
-              data={chartData} 
-              options={options}
-              height={500}
-              width= {1000}
-            />
-        </div>
+      <Fragment>
+          <div className="flex justify-around items-center">
+
+          </div>
+
+          {/* <Bar 
+            data={chartData} 
+            options={options}
+            height={500}
+            width= {1000}
+          /> */}
+      </Fragment>
     )
 }
 
